d9fa45e55ce7712764b3c55f74c9049ba6c5caad
rajag <rajagurru333@gmail.com>
2024-01-23 09:23:37 +0530

    chan

--- models/customers.js
+++ models/customers.js
@@ -11,14 +11,14 @@ const customersSchema = new mongoose.Schema(
             type: String,
             required: true
         },
-        Address: {
-            type: String,
-            required: true
-        },
-        location: {
-            type: String,
-            required: true
-        },
+        // Address: {
+        //     type: String,
+        //     required: true
+        // },
+        // location: {
+        //     type: String,
+        //     required: true
+        // },
         Number: {
             type: Number,
             required: true
@@ -27,6 +27,31 @@ const customersSchema = new mongoose.Schema(
             type: String,
             required: true
         },
+        Age: {
+            type: String,
+            required: true
+        },
+        Gender: {
+            type: String,
+            required: true
+        },
+        Address: {
+            type: String,
+            required: true
+        },
+        City: {
+            type: String,
+            required: true
+        },
+        State: {
+            type: String,
+            required: true
+        },
+        Pincode: {
+            type: String,
+            required: true
+        },
+        
         // Password: {
         //     type: String,
         //     required: true
